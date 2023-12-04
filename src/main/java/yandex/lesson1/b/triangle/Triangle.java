14e0a302d939daed85f72afbd29693753115739c
alexei1volf <alexei1volf@gmail.com>
2023-12-04 14:26:13 +0200

    yandex lesson1 b input/output

--- src/main/java/yandex/lesson1/b/triangle/Triangle.java
+++ src/main/java/yandex/lesson1/b/triangle/Triangle.java
@@ -1,8 +1,19 @@
 package yandex.lesson1.b.triangle;
 
+import java.util.Scanner;
+
 public class Triangle
 {
-    String validate(int a, int b, int c) {
+    public static void main(String[]args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println(validate(a, b, c));
+    }
+
+    static String validate(int a, int b, int c) {
         boolean isValid = (a < b + c) && (a > Math.abs(b - c));
         if (isValid) {
             return "YES";
