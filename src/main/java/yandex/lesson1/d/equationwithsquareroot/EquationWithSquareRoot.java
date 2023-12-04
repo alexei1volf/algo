6a92ba61d41a71444394a03b868cbc910addce04
alexei1volf <alexei1volf@gmail.com>
2023-12-04 14:42:34 +0200

    yandex lesson1 d input/output

--- src/main/java/yandex/lesson1/d/equationwithsquareroot/EquationWithSquareRoot.java
+++ src/main/java/yandex/lesson1/d/equationwithsquareroot/EquationWithSquareRoot.java
@@ -1,8 +1,19 @@
 package yandex.lesson1.d.equationwithsquareroot;
 
+import java.util.Scanner;
+
 public class EquationWithSquareRoot
 {
-    public String calculate(int a, int b, int c) {
+    public static void main(String[]args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println(calculate(a, b, c));
+    }
+
+    public static String calculate(int a, int b, int c) {
         if (c < 0) {
             return "NO SOLUTIONS";
         }
