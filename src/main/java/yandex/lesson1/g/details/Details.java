e77341a8cbb34ac6ff241b9fd83aec3041defb29
alexei1volf <alexei1volf@gmail.com>
2023-12-08 14:57:13 +0200

    yandex lesson1 g details

---
+++ src/main/java/yandex/lesson1/g/details/Details.java
@@ -0,0 +1,31 @@
+package yandex.lesson1.g.details;
+
+import java.util.Scanner;
+
+public class Details
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int m = sc.nextInt();
+        int num = findDetails(n, k, m);
+        System.out.println(num);
+    }
+
+    public static int findDetails(int n, int k, int m) {
+        int met = n;
+        int det = 0;
+        while (met / k > 0) {
+            int met1 = met % k;
+            int zag = met / k;
+
+            int met2 = (k % m) * zag;
+            det = det + ((k / m) * zag);
+            met = met1 + met2;
+        }
+
+        return det;
+    }
+}
---
+++ src/test/java/yandex/lesson1/g/details/DetailsTest.java
@@ -0,0 +1,45 @@
+package yandex.lesson1.g.details;
+
+import org.junit.jupiter.api.Test;
+import yandex.lesson1.f.laptops.Laptops;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DetailsTest
+{
+
+    @Test
+    void test_findDetails_case1() {
+        int n = 10;
+        int k = 5;
+        int m = 2;
+
+        int result = Details.findDetails(n, k, m);
+
+        assertEquals(4, result);
+    }
+
+    @Test
+    void test_findDetails_case2() {
+        int n = 13;
+        int k = 5;
+        int m = 3;
+
+        int result = Details.findDetails(n, k, m);
+
+        assertEquals(3, result);
+    }
+
+    @Test
+    void test_findDetails_case3() {
+        int n = 14;
+        int k = 5;
+        int m = 3;
+
+        int result = Details.findDetails(n, k, m);
+
+        assertEquals(4, result);
+    }
+}
